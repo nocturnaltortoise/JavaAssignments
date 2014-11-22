e52b78d514cc211e9ef17a6af7cc5f19116ad8ff
Simon Turner <simonturner34@gmail.com>
2014-11-22 21:27:46 +0000

    reducing number of arrays

--- com1003assignment2/PictureV2.java
+++ com1003assignment2/PictureV2.java
@@ -5,9 +5,10 @@ public class PictureV2{
 	public enum Colour{BLUE, GREEN, DARK_GREEN, BROWN}
 	private static Colour colour;
 
-	private static final int SCREEN_SIZE = 600;
-	private static final int SKETCH_SCREEN_SIZE = 200;
 	private static final int BLOCK_SIZE = 3;
+	private static final int SCREEN_SIZE = 200 * BLOCK_SIZE;
+	private static final int IMAGE_SIZE = 200;
+	private static final int SKETCH_SCREEN_SIZE = 200;
 	private static EasyGraphics mainWindow = new EasyGraphics(SCREEN_SIZE, SCREEN_SIZE);
 	private static EasyGraphics sketchWindow = new EasyGraphics(SKETCH_SCREEN_SIZE, SKETCH_SCREEN_SIZE);
 
@@ -15,33 +16,28 @@ public class PictureV2{
 
 		EasyReader fileInput = new EasyReader("picture.txt");
 		String input = fileInput.readString();
-		int[] digitArray = new int[40000];
 
-		for(int i = 0; i < input.length(); i++){
+		Colour[][] colourArray = new Colour[IMAGE_SIZE][IMAGE_SIZE];
+		int digitNumber = 0;
 
-			digitArray[i] = Integer.parseInt(input.substring(i, i+1));
+		for(int y = 0; y < IMAGE_SIZE; y++){
 
-		}
+			for(int x = 0; x < IMAGE_SIZE; x++){
+				digitNumber++;
 
-		int[][] imageArray = new int[200][200];
-		Colour[][] colourArray = new Colour[200][200];
-
-		for(int y = 0; y < 200; y++){
-
-			for(int x = 0; x < 200; x++){
-				imageArray[x][y] = digitArray[x + (y*200)];
-				colourArray[x][y] = checkColour(imageArray[x][y]);
+				colourArray[x][y] = checkColour(Integer.parseInt(input.substring(digitNumber, digitNumber+1)));
 			
 				setRGB(colourArray[x][y]);
-				mainWindow.fillRectangle(x*BLOCK_SIZE,SCREEN_SIZE - (y * BLOCK_SIZE),BLOCK_SIZE,BLOCK_SIZE);
-				mainWindow.drawRectangle(x*BLOCK_SIZE,SCREEN_SIZE - (y*BLOCK_SIZE), BLOCK_SIZE, BLOCK_SIZE);
+				mainWindow.drawRectangle(x * BLOCK_SIZE, SCREEN_SIZE - (y * BLOCK_SIZE), BLOCK_SIZE, BLOCK_SIZE);
+				mainWindow.fillRectangle(x * BLOCK_SIZE, SCREEN_SIZE - (y * BLOCK_SIZE), BLOCK_SIZE, BLOCK_SIZE);
+
 			}	
 
 		}
 
-		for(int y = 1; y < 199; y++){
+		for(int y = 1; y < IMAGE_SIZE - 1; y++){
 
-			for(int x = 1; x < 199; x++){
+			for(int x = 1; x < IMAGE_SIZE - 1; x++){
 				
 				if(colourArray[x][y] != colourArray[x][y-1]){
 					sketchWindow.plot(x,SKETCH_SCREEN_SIZE - y);
@@ -80,6 +76,9 @@ public class PictureV2{
 				break;
 			case BROWN:
 				mainWindow.setColor(150,100,90);
+				break;
+			default:
+				mainWindow.setColor(0,0,0);
 		}		
 
 	}
