97469df35429585ca8c0467f349e3da3b095cdc1
Simon Turner <STurner4@sheffield.ac.uk>
2014-11-27 14:00:47 +0000

    changing variable names

--- com1003assignment2/Picture.java
+++ com1003assignment2/Picture.java
@@ -15,18 +15,18 @@ public class Picture{
 		String input = fileInput.readString();
 
 		//image is a square of 40,000 pixels - i.e. side length 200 (or sqrt(40,000)).
-		final int IMAGE_SIZE = (int)Math.sqrt(input.length());
+		final int IMAGE_SIDE_LENGTH = (int)Math.sqrt(input.length());
 		//screen size is 600, because each block is 3 pixels on screen
 		final int SCREEN_SIZE = 600;
 		//sketch screen size just needs to be smaller so the colour image is still viewable.
 		final int SKETCH_SCREEN_SIZE = 200;
 		
-		renderColourImage(input, colourArray, SCREEN_SIZE, IMAGE_SIZE);
-		renderSketch(colourArray, SKETCH_SCREEN_SIZE, IMAGE_SIZE);
+		renderColourImage(input, colourArray, SCREEN_SIZE, IMAGE_SIDE_LENGTH);
+		renderSketch(colourArray, SKETCH_SCREEN_SIZE, IMAGE_SIDE_LENGTH);
 		
 	}
 
-	private static void renderColourImage(String input, Colour[][] colourArray, final int SCREEN_SIZE, final int IMAGE_SIZE){
+	private static void renderColourImage(String input, Colour[][] colourArray, final int SCREEN_SIZE, final int IMAGE_SIDE_LENGTH){
 		
 		final int BLOCK_SIZE = 3;
 		EasyGraphics mainWindow = new EasyGraphics(SCREEN_SIZE, SCREEN_SIZE);
@@ -34,16 +34,19 @@ public class Picture{
 		//counter that increments up to the size of the data making the image (40,000 digits in this case)
 		int digitNumber = 0;
 
-		for(int y = 0; y < IMAGE_SIZE; y++){
+		for(int y = 0; y < IMAGE_SIDE_LENGTH; y++){
 
-			for(int x = 0; x < IMAGE_SIZE; x++){
+			for(int x = 0; x < IMAGE_SIDE_LENGTH; x++){
 				//works out the current digit in the input file
 				int currentDigit = Integer.parseInt(input.substring(digitNumber, digitNumber+1));
 				//populates an array of enums with the Colour returned by checkColour for that digit in the file
 				colourArray[x][y] = checkColour(currentDigit);
 				
+				int redValue = getRGB(colourArray[x][y])[0];
+				int greenValue = getRGB(colourArray[x][y])[1];
+				int blueValue = getRGB(colourArray[x][y])[2];
 				//sets the colour for drawing based on the enum value for that pixel
-				mainWindow.setColor(getRGB(colourArray[x][y])[0], getRGB(colourArray[x][y])[1], getRGB(colourArray[x][y])[2]);
+				mainWindow.setColor(redValue, greenValue, blueValue);
 				//each rectangle is scaled up by the block size, with the y coordinate flipped so the picture is the right orientation
 
 				mainWindow.fillRectangle(x * BLOCK_SIZE, SCREEN_SIZE - (y * BLOCK_SIZE), BLOCK_SIZE, BLOCK_SIZE);
@@ -55,17 +58,17 @@ public class Picture{
 
 	}
 
-	private static void renderSketch(Colour[][] colourArray, final int SKETCH_SCREEN_SIZE, final int IMAGE_SIZE){
+	private static void renderSketch(Colour[][] colourArray, final int SKETCH_SCREEN_SIZE, final int IMAGE_SIDE_LENGTH){
 
 		EasyGraphics sketchWindow = new EasyGraphics(SKETCH_SCREEN_SIZE, SKETCH_SCREEN_SIZE);
 
 		/* nested for loop to plot points where colours change between blocks in the image
-		 * starts at 1 and ends at IMAGE_SIZE - 1 so it doesn't either go outside the range of the array
+		 * starts at 1 and ends at IMAGE_SIDE_LENGTH - 1 so it doesn't either go outside the range of the array
 		 * or detect the edges of the screen. 
 		 */
-		for(int y = 1; y < IMAGE_SIZE - 1; y++){
+		for(int y = 1; y < IMAGE_SIDE_LENGTH - 1; y++){
 
-			for(int x = 1; x < IMAGE_SIZE - 1; x++){
+			for(int x = 1; x < IMAGE_SIDE_LENGTH - 1; x++){
 				
 				//checks up, down, left and right in the image (200x200) to see whether the enum value for that block differs
 				if(colourArray[x][y] != colourArray[x][y-1]){
